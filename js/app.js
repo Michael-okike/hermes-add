c36870d2434c582082a0fb31875830fa39e4d996
Michael-okike <michaelokike57@gmail.com>
2022-06-04 00:49:13 +0800

    sections

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// function toggleSection(){
+//     let hide = document.getElementById('view-more');
+//     let displaySetting = hide.style.display;
+//     let sectionButton = document.getElementById('sectionButton');
+//     // sectionButton.addEventListener('click',) 
+//     //     sectionButton.style.display = "none";
+    
+//     if (displaySetting == 'block') {
+//         hide.style.display = 'none';
+//         sectionButton.innerHTML = 'Scroll up'
+//     }
+//     else{
+//         hide.style.display = 'block';
+//         sectionButton.innerHTML = "View More"
+//     }
+
+
+// }
+
+
